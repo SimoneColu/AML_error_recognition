ef45f376433b538619d18cb4b1e85a6ee982f3dd
SIMONE COLUMBARO s343584 <columbaro.simone@gmail.com>
2026-01-06 13:16:03 +0100

    minor fix

--- train_tv.py
+++ train_tv.py
@@ -71,6 +71,7 @@ def train_task_verification_loop(config):
         # Addestriamo per N epoche su (K-1) ricette
         model.train()
         for epoch in range(config.num_epochs):
+            epoch_loss = 0.0
             for batch in train_loader:
                 features, labels, masks, _ = batch
                 
